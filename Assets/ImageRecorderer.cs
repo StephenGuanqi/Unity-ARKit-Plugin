f15d19a0331fe5f06f39dd43bfd78ea6b23f1218
Guanqi Yu <guanqiy@andrew.cmu.edu>
2017-07-03 21:52:19 +0800

    modified plugin source for projects extension

---
+++ Assets/ImageRecorderer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.iOS;
+
+public class ImageRecorderer : MonoBehaviour {
+
+	private int recordMode = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeRecordingState() {
+		if (recordMode == 0) {
+			recordMode = 1;
+			GameObject.Find("Button").GetComponentInChildren<Text>().text = "is recording.....";
+			UnityARSessionNativeInterface.GetARSessionNativeInterface ().setARRecordingState (recordMode);
+		} else {
+			recordMode = 0;
+			GameObject.Find("Button").GetComponentInChildren<Text>().text = "Start Recording";
+			UnityARSessionNativeInterface.GetARSessionNativeInterface ().setARRecordingState (recordMode);
+		}
+	}
+
+	
+}
---
+++ Assets/ZombieControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieControl : MonoBehaviour {
+
+	private Animation animation;
+	private bool shouldMove = false;
+
+	// Use this for initialization
+	void Start () {
+		animation = GetComponent<Animation> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (shouldMove) {
+
+			transform.Translate (Vector3.forward * Time.deltaTime * (transform.localScale.x * .05f));
+
+		}
+	}
+
+	public void Walk() {
+		if (!animation.isPlaying) {
+			animation.Play ();
+				shouldMove = true;
+		} else {
+			animation.Stop ();
+			shouldMove = false;
+		}
+	}
+
+	public void lookAt() {
+		transform.LookAt(Camera.main.transform.position);
+		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+	}
+	
+	public void Bigger() {
+		
+		transform.localScale += new Vector3 (1, 1, 1);
+
+	}
+
+	public void Smaller() {
+		if (transform.localScale.x > 1) {
+			transform.localScale -= new Vector3 (1, 1, 1);
+		}
+	}
+
+}
